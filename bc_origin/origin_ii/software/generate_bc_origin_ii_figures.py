c50aa9fd3ac1447792610324fdc5fef268daa341
AlexMalachevsky <t89257409519@gmail.com>
2026-06-24 13:32:09 +0300

    Add BC-Origin II figure generator scaffold

---
+++ bc_origin/origin_ii/software/generate_bc_origin_ii_figures.py
@@ -0,0 +1,17 @@
+# BC-Origin II figure generator scaffold
+# Full version will be populated from validated prototype
+
+"""
+Generates BC-Origin II structural kernel diagnostics.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    print('BC-Origin II generator scaffold')
+
+
+if __name__ == '__main__':
+    main()
