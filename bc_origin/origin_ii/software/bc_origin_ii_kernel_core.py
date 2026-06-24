b7f9407ce4f4e7b6bc2e6f1280a329434f58b1d3
AlexMalachevsky <t89257409519@gmail.com>
2026-06-24 13:31:45 +0300

    Add BC-Origin II kernel core scaffold

---
+++ bc_origin/origin_ii/software/bc_origin_ii_kernel_core.py
@@ -0,0 +1,18 @@
+# BC-Origin II kernel core scaffold
+# (will be replaced with full implementation)
+
+"""
+BC-Origin II: Structural Coupling Kernels and Multi-Shadow Geometry
+Initial scaffold file.
+"""
+
+from __future__ import annotations
+
+import numpy as np
+
+
+def sign(x):
+    return 1 if x > 0 else -1 if x < 0 else 0
+
+
+# TODO: migrate full implementation from local validated prototype
